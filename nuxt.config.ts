974ee77feccbff8d0f79d1730826b7be03cf5168
mkieler <mattias@mkieler.com>
2025-07-08 10:56:49 +0200

    Use .env to set sanctum basurl instead of configfile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,6 @@ export default defineNuxtConfig({
   
 
   sanctum: {
-    baseUrl: 'http://localhost:8000', // Laravel API
     redirect: {
         keepRequestedRoute: false,
         onAuthOnly: '/login',
